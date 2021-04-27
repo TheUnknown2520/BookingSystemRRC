604cd06b7442c1a56c6629e28dc68f2da9b6c1a9
ChrispyChrisTheChrisp <45943684+TheUnknown2520@users.noreply.github.com>
2021-04-27 09:23:31 +0200

    Made Guests and Models Also created properties in Booking

---
+++ BookingSystemRRC/Models/Guest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystemRRC.Models
+{
+    public class Guest
+    {
+        public int BookingId { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Comment { get; set; }
+    }
+}
--- BookingSystemRRC/Pages/Booking.cshtml.cs
+++ BookingSystemRRC/Pages/Booking.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookingSystemRRC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -17,8 +18,11 @@ namespace BookingSystemRRC.Pages
             _logger = logger;
         }
 
+        public Guest Guest { get; set; }
+
         public void OnGet()
         {
+            Guest = new Guest() { BookingId = 1, Name = "Christopher", Address = "Østerdalsgade 12", Email = "Chrisseria2520@gmail.com", PhoneNumber = 31353551, Comment = "Der skal stå øl klar" };
         }
     }
 }
