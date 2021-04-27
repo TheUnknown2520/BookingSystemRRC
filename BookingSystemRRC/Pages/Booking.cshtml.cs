60197c77bfe7b6bbe29451c69c49e7fa7be597c0
ChrispyChrisTheChrisp <45943684+TheUnknown2520@users.noreply.github.com>
2021-04-27 13:44:42 +0200

    DONT PULL

--- BookingSystemRRC/Pages/Booking.cshtml.cs
+++ BookingSystemRRC/Pages/Booking.cshtml.cs
@@ -22,7 +22,7 @@ namespace BookingSystemRRC.Pages
 
         public void OnGet()
         {
-            Guest = new Guest() { BookingId = 1, Name = "Christopher", Address = "Østerdalsgade 12", Email = "Chrisseria2520@gmail.com", PhoneNumber = 31353551, Comment = "Der skal stå øl klar" };
+            Guest = new Guest() {  };
         }
     }
 }
