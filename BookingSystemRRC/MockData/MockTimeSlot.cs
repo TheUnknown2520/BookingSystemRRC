0f2096e286366e3705eb94aca605797618c7b0d2
Casper Hesseldahl Sølling Christensen <casp266g@edu.easj.dk>
2021-05-21 21:46:49 +0200

    Små ændringer på design lavet fredag aften

--- BookingSystemRRC/MockData/MockTimeSlot.cs
+++ BookingSystemRRC/MockData/MockTimeSlot.cs
@@ -8,19 +8,19 @@ namespace BookingSystemRRC.MockData
 {
     public class MockTimeSlot
     {
-        private static List<Timeslot> timeslots = new List<Timeslot>
-        {
-            new Timeslot("10:30", "11:30"),
-            new Timeslot("11:30", "12:30"),
-            new Timeslot("12:30", "13:30"),
-            new Timeslot("13:30", "14:30"),
-            new Timeslot("14:30", "15:30"),
-            new Timeslot("15:30", "16:30")
-        };
+        //private static List<Timeslot> timeslots = new List<Timeslot>
+        //{
+        //    new Timeslot("10:30", "11:30"),
+        //    new Timeslot("11:30", "12:30"),
+        //    new Timeslot("12:30", "13:30"),
+        //    new Timeslot("13:30", "14:30"),
+        //    new Timeslot("14:30", "15:30"),
+        //    new Timeslot("15:30", "16:30")
+        //};
 
-        public static List<Timeslot> GetMockUsers()
-        {
-            return timeslots;
-        }
+        //public static List<Timeslot> GetMockUsers()
+        //{
+        //    return timeslots;
+        //}
     }
 }
